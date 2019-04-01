46ce9ebcd4cb2b674d43698e82a748032d41a1db
David Banks <amoebae@gmail.com>
2019-04-01 12:49:57 +0100

    will test connection to local graphql endpoint

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,4 +1,10 @@
 import pytest
 
+def check_is_open(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((host, port))
+    return result == 0
+
+
 def test_connection():
-    assert True
+    assert check_is_open('localhost', 8080)
