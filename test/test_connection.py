b88b4d1526fc65ca99c4af7facb109509581c125
David Banks <amoebae@gmail.com>
2019-04-03 10:53:44 +0100

    retrieve graphiql

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,5 +1,6 @@
 import pytest
 import socket
+import requests
 
 def check_is_open(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,3 +9,7 @@ def check_is_open(host, port):
 
 def test_connection():
     assert check_is_open('localhost', 8080)
+
+def test_get_graphiql():
+    response = requests.get('http://localhost:8080/static/graphiql')
+    assert response.status_code == 200
