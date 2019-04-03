31d777f5a9d63edda7e1c0d8ffd3f001197ba40e
David Banks <amoebae@gmail.com>
2019-04-03 11:19:52 +0100

    wait for it to come up

--- test/test_connection.py
+++ test/test_connection.py
@@ -7,9 +7,17 @@ def check_is_open(host, port):
     result = sock.connect_ex((host, port))
     return result == 0
 
-def test_connection():
-    assert check_is_open('localhost', 8080)
+def wait_until_open(host, port, timeout):
+    elapsed = 0
+    while not check_is_open(host, port):
+        time.sleep(1)
+        elapsed += 1
+        if elapsed > timeout:
+            raise Exception('timed out waiting for server to come up')
+    return elapsed
 
-# def test_get_graphiql():
-#     response = requests.get('http://localhost:8080/static/graphiql')
-#     assert response.status_code == 200
+def test_get_graphiql():
+    elapsed = wait_until_open('localhost', 8080, 60)
+    print("Waited", elapsed, "seconds for server to come up")
+    response = requests.get('http://localhost:8080/static/graphiql')
+    assert response.status_code == 200
