4489eea8dc325dca8c075338db19682e42eca109
David Banks <amoebae@gmail.com>
2019-04-03 10:49:53 +0100

    refactor tests

--- test/test_query.py
+++ test/test_query.py
@@ -1,6 +1,26 @@
 import pytest
 import pprint
+import dsim.query_clusius
 
-def test_query_dataset(previously_uploaded_dataset):
-    pprint.pprint(previously_uploaded_dataset)
-    assert True
+# def test_query_dataset(previously_uploaded_dataset):
+#     pprint.pprint(previously_uploaded_dataset)
+#     assert True
+
+
+EXPECTED_RESULT = {
+    'data': {
+        'dataSets': {
+            'u33707283d426f900d4d33707283d426f900d4d0d__biaclusius': {
+                'clusius_Persons': {
+                    'tim_gender': {
+                        'value': 'MALE'
+                    }
+                }
+            }
+        }
+    }
+}
+
+def test_query():
+    result = dsim.query_clusius.do_query()
+    assert result == EXPECTED_RESULT
